160e82b5d68b02feef5b71b6872dd4ffbb3a1455
zambia1972 <qatan.1972@gmail.com>
2025-05-08 11:04:10 +0200

    Update main.user.js

--- userscript/main.user.js
+++ userscript/main.user.js
@@ -6,9 +6,9 @@
 // @match        *://*.grepolis.com/*
 // @grant        GM_addStyle
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @require      https://raw.githubusercontent.com/jouwgebruikersnaam/GrepoBot/main/userscript/functions.js
-// @require      https://raw.githubusercontent.com/jouwgebruikersnaam/GrepoBot/main/userscript/api.js
-// @require      https://raw.githubusercontent.com/jouwgebruikersnaam/GrepoBot/main/userscript/ui.js
+// @require      https://github.com/zambia1972/grepogold/blob/main/userscript/functions.js
+// @require      https://github.com/zambia1972/grepogold/blob/main/userscript/api.js
+// @require      https://github.com/zambia1972/grepogold/blob/main/userscript/ui.js
 // ==/UserScript==
 
 (function () {
